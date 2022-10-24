cb1bde241d12401beeffca2683bedc097ca2608e
shivral somani <shivral312002@gmail.com>
2022-10-24 15:25:40 +0530

    added urls

--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+def api_home(request ,*args, **kwargs):
+    return JsonResponse({"Message" :"this is inside"})
 
 # Create your views here.
