2d504dc984b493d142f13394339a823bc46090bd
shivral somani <shivral312002@gmail.com>
2022-10-26 19:34:32 +0530

    jwt gen 1

--- restapi/serializers.py
+++ restapi/serializers.py
@@ -1,14 +1,32 @@
+from asyncore import write
+from tkinter import N
 from rest_framework import serializers
 
-from restapi.models import Actor, Hall
+from restapi.models import User
 
-class HallSerializer(serializers.ModelSerializer):
+# class HallSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=Hall
+#         fields='__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Hall
+        model=User
         fields='__all__'
+    
 
-
-class ActorSerializer(serializers.ModelSerializer):
+class USerLoginSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Actor
-        fields='__all__'
+        model=User
+        fields=['id','email','password','name']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+    def create(self,validated_data):
+        password=validated_data.pop('password',None)
+        instance=self.Meta.model(**validated_data)
+        if password!=None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- restapi/urls.py
+++ restapi/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path,re_path
-from .views import api_home
+from .views import api_home, logout,register,login, userView
 urlpatterns = [
-    path('home',api_home)
+    path('home',api_home),
+    path('register',register),
+    path('login',login),
+    path('user',userView),
+    path('logout',logout)
 ]
