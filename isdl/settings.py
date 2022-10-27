99fe6d6ffe1956296b77daffc7e65b772e3ad0d5
shivral somani <shivral312002@gmail.com>
2022-10-27 21:41:22 +0530

    res

--- isdl/settings.py
+++ isdl/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOW_ALL_ORIGINS = True  
 CORS_ALLOW_CREDENTIALS=True
 AUTH_USER_MODEL='restapi.User'
 
